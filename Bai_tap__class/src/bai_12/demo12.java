9e3e0ae92d80c8cc4513a17ed17215c3928701a2
PNamPD <phungnam151094@gmail,com>
2019-03-29 17:53:11 +0700

    bai12class

--- Bai_tap__class/src/bai_12/demo12.java
+++ Bai_tap__class/src/bai_12/demo12.java
@@ -5,10 +5,43 @@
  */
 package bai_12;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ASUS
  */
 public class demo12 {
-    
+
+    public static void main(String[] args){
+        daGiac test = new daGiac();
+        float a,b;
+        Scanner sc= new Scanner(System.in);
+        int choice;
+        System.out.println("Chon tinh dien tich hinh:");
+        System.out.println("1.Hinh binh hanh");
+        System.out.println("2.Hinh chu nhat");
+        System.out.println("3.Hinh vuong:");
+        choice = sc.nextInt();
+        switch(choice){
+            case 1: {
+                test = new HinhBinhHanh();
+                break;
+            }
+            case 2: {
+                test= new HinhChuNhat();
+                break;
+            }
+            case 3: {
+                test= new HinhVuong();
+                break;
+            }
+        }
+        System.out.println("Nhap canh a: ");
+        a=sc.nextFloat();
+        System.out.println("Nhap canh b: ");
+        b=sc.nextFloat();
+        test.tinhChuVi(a,b);
+        test.tinhDienTich(a,b);
+    }
 }
